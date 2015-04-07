720abd3ddd833c00a14ada07cf5e95ee16e9556d
akos.sereg <akos.sereg@gmail.com>
2015-04-07 22:34:26 +0200

    Date range picker: Adjusting to beginning/end of month (+1 -1 month buttons)

--- public/resources/js/view/DateRangePicker.js
+++ public/resources/js/view/DateRangePicker.js
@@ -40,6 +40,34 @@ var dateRangePicker = Ext.create('Ext.form.FormPanel', {
     },
 
     addMonths: function(months) {
+
+        var startDate = Ext.getCmp('startDateControl').getValue();
+        var endDate = Ext.getCmp('endDateControl').getValue();
+
+        if (months == -1 && startDate.getDate() == 1) {
+            // Going back to one month (adjusting to the beginning/end of the month)
+            startDate.setDate(0);   // Last day of previous month
+            endDate = new Date(startDate);  // ... will be the end date
+            startDate.setDate(1);   // Set start date to the beginning of the month
+
+            Ext.getCmp('startDateControl').setValue(startDate);
+            Ext.getCmp('endDateControl').setValue(endDate);
+
+            this.onRangeChanged();
+            return;
+        }
+        else if (months == 1 && startDate.getDate() == 1) {
+            // Going one month forward (adjusting to the beginning/end of the month)
+            startDate = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 1);
+            endDate = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0);
+
+            Ext.getCmp('startDateControl').setValue(startDate);
+            Ext.getCmp('endDateControl').setValue(endDate);
+
+            this.onRangeChanged();
+            return;
+        }
+
         Ext.getCmp('startDateControl').setValue(Ext.Date.add(Ext.getCmp('startDateControl').getValue(), Ext.Date.MONTH, months));
         Ext.getCmp('endDateControl').setValue(Ext.Date.add(Ext.getCmp('endDateControl').getValue(), Ext.Date.MONTH, months));
 
