f1f36aafb27b3536d935862898f2df50888609c8
akos.sereg <akos.sereg@gmail.com>
2015-04-13 00:53:39 +0200

    Highlight buttons on navigation panel based on active panel

--- public/resources/js/view/Navigation.js
+++ public/resources/js/view/Navigation.js
@@ -4,28 +4,51 @@ var navigationPanel = Ext.create('Ext.form.Panel', {
     margins: '0 0 0 0',
     items: [{
         xtype: 'button',
+        id: 'expensesBtn',
         icon: '/resources/images/bar_chart.png',
         text: 'Expenses',
         scale: 'medium',
         anchor: '100%',
         handler : function() {
-            expensesPanel.getEl().show();
-            tagAssociationsPanel.getEl().hide();
+            navigationPanel.navigate('expense');
         }
     },
     {
         xtype: 'button',
+        id: 'tagAssocBtn',
         icon: '/resources/images/icon-tags.png',
         text: 'Tag associations',
         scale: 'medium',
         anchor: '100%',
         margin: '10 0 0 0',
-
         handler : function() {
-
-            expensesPanel.getEl().hide();
-            tagAssociationsPanel.getEl().show();
+            navigationPanel.navigate('tagAssociations');
         }
     }],
+
+    navigate: function(tabName) {
+
+        // Hide all
+        expensesPanel.getEl().hide();
+        Ext.getCmp('expensesBtn').setIcon('/resources/images/bar_chart_off.png');
+        Ext.getCmp('expensesBtn').getEl().dom.style.background = '#eeeeee';
+
+        tagAssociationsPanel.getEl().hide();
+        Ext.getCmp('tagAssocBtn').setIcon('/resources/images/icon-tags_off.png');
+        Ext.getCmp('tagAssocBtn').getEl().dom.style.background = '#eeeeee';
+
+        switch(tabName) {
+            case 'expense':
+                expensesPanel.getEl().show();
+                Ext.getCmp('expensesBtn').setIcon('/resources/images/bar_chart.png');
+                Ext.getCmp('expensesBtn').getEl().dom.style.background = '#dddddd';
+                break;
+            case 'tagAssociations':
+                tagAssociationsPanel.getEl().show();
+                Ext.getCmp('tagAssocBtn').setIcon('/resources/images/icon-tags.png');
+                Ext.getCmp('tagAssocBtn').getEl().dom.style.background = '#dddddd';
+                break;
+        }
+    }
 
 });
